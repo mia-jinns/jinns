b6142aadd0a0d2b4316e3ac75b12f7dc0a425ea9
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2023-09-11 16:36:21 +0200

    Add RAR for other types for PDEStatio and PDE non statio

--- jinns/solver/_rar.py
+++ jinns/solver/_rar.py
@@ -23,8 +23,7 @@ def rar_step_init(sample_size, selected_sample_size):
 
         if isinstance(data, DataGeneratorODE):
             s = data.sample_in_time_domain(sample_size)
-            _, loss_dict = loss_evaluate_fun(params, s, reduction=None)
-            mse_on_s = loss_dict["dyn_loss"]
+            mse_on_s = loss_evaluate_fun(params, s, reduction=None, dyn_only=True)
             ## Select the m points with higher dynamic loss
             higher_residual_idx = jax.lax.dynamic_slice(
                 jnp.argsort(mse_on_s),
@@ -43,7 +42,7 @@ def rar_step_init(sample_size, selected_sample_size):
                 higher_residual_points,
                 (data.rar_parameters["iter_nb"] * selected_sample_size,),
             )
-            # jax.debug.print("new points={p}", p=higher_residual_points)
+
             ## rearrange probabilities so that the probabilities of the new
             ## points are non-zero
             new_proba = 1 / (
@@ -63,7 +62,6 @@ def rar_step_init(sample_size, selected_sample_size):
             data.p = jax.lax.fori_loop(
                 0, data.rar_parameters["iter_nb"], update_slices, data.p
             )
-            # jax.debug.print("[RAR sampling] non zero proba={p}", p=jnp.count_nonzero(data.p))
 
             data.rar_parameters["iter_nb"] += 1
 
@@ -71,11 +69,118 @@ def rar_step_init(sample_size, selected_sample_size):
             # have side effects in this function that will be jitted
             return data
 
-        elif isinstance(data, CubicMeshPDEStatio):
-            raise NotImplementedError
+        elif isinstance(data, CubicMeshPDEStatio) and not isinstance(
+            data, CubicMeshPDENonStatio
+        ):
+            s = data.sample_in_omega_domain(sample_size)
+            mse_on_s = loss_evaluate_fun(params, s, reduction=None, dyn_only=True)
+            ## Select the m points with higher dynamic loss
+            higher_residual_idx = jax.lax.dynamic_slice(
+                jnp.argsort(mse_on_s),
+                (mse_on_s.shape[0] - selected_sample_size,),
+                (selected_sample_size,),
+            )
+            higher_residual_points = s[higher_residual_idx]
+
+            data.rar_parameters["iter_from_last_sampling"] = 0
+
+            ## add the new points in times
+            # start indices of update can be dynamic but the the shape (length)
+            # of the slice
+            data.omega = jax.lax.dynamic_update_slice(
+                data.omega,
+                higher_residual_points,
+                (data.rar_parameters["iter_nb"] * selected_sample_size,),
+            )
+
+            ## rearrange probabilities so that the probabilities of the new
+            ## points are non-zero
+            new_proba = 1 / (
+                data.n_start + data.rar_parameters["iter_nb"] * selected_sample_size
+            )
+            # the next work because n_start is static
+            data.p = data.p.at[: data.n_start].set(new_proba)
+
+            # the next requires a fori_loop because the range is dynamic
+            def update_slices(i, p):
+                return jax.lax.dynamic_update_slice(
+                    p,
+                    1 / new_proba * jnp.ones((selected_sample_size,)),
+                    ((data.n_start + (i + 1) * selected_sample_size),),
+                )
+
+            data.p = jax.lax.fori_loop(
+                0, data.rar_parameters["iter_nb"], update_slices, data.p
+            )
+
+            data.rar_parameters["iter_nb"] += 1
+
+            # NOTE must return data to be correctly updated because we cannot
+            # have side effects in this function that will be jitted
+            return data
 
         elif isinstance(data, CubicMeshPDENonStatio):
-            raise NotImplementedError
+            st = data.sample_in_time_domain(sample_size)
+            sx = data.sample_in_omega_domain(sample_size)
+            mse_on_s = loss_evaluate_fun(
+                params,
+                (sx, None, st),
+                reduction=None,
+                dyn_only=True,
+                dyn_cartesian_product=False,
+            ).flatten()
+            ## Select the m points with higher dynamic loss
+            higher_residual_idx = jax.lax.dynamic_slice(
+                jnp.argsort(mse_on_s),
+                (mse_on_s.shape[0] - selected_sample_size,),
+                (selected_sample_size,),
+            )
+            higher_residual_points_st = st[higher_residual_idx]
+            higher_residual_points_sx = sx[higher_residual_idx]
+
+            data.rar_parameters["iter_from_last_sampling"] = 0
+
+            ## add the new points in times
+            # start indices of update can be dynamic but the the shape (length)
+            # of the slice
+            data.times = jax.lax.dynamic_update_slice(
+                data.times,
+                higher_residual_points_st,
+                (data.rar_parameters["iter_nb"] * selected_sample_size,),
+            )
+
+            ## add the new points in omega
+            data.omega = jax.lax.dynamic_update_slice(
+                data.omega,
+                higher_residual_points_sx,
+                (data.rar_parameters["iter_nb"] * selected_sample_size, data.dim),
+            )
+
+            ## rearrange probabilities so that the probabilities of the new
+            ## points are non-zero
+            new_proba = 1 / (
+                data.n_start + data.rar_parameters["iter_nb"] * selected_sample_size
+            )
+            # the next work because nt_start is static
+            data.p = data.p.at[: data.n_start].set(new_proba)
+
+            # the next requires a fori_loop because the range is dynamic
+            def update_slices(i, p):
+                return jax.lax.dynamic_update_slice(
+                    p,
+                    1 / new_proba * jnp.ones((selected_sample_size,)),
+                    ((data.n_start + (i + 1) * selected_sample_size),),
+                )
+
+            data.p = jax.lax.fori_loop(
+                0, data.rar_parameters["iter_nb"], update_slices, data.p
+            )
+
+            data.rar_parameters["iter_nb"] += 1
+
+            # NOTE must return data to be correctly updated because we cannot
+            # have side effects in this function that will be jitted
+            return data
 
     def rar_step_false(operands):
         loss_evaluate_fun, params, data, i = operands
