26abf1f006222f7e57446639af376918818b3ed4
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2023-11-22 17:34:11 +0100

    correct the script to perform tests. We were returning 0 always and not catching any failure on the docker (lol)

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 pytest dataGenerator_tests/*
+if [ $? -ne 0 ]; then
+   echo 'ERROR: pytest failed, exiting ...'
+   exit $?
+fi
 pytest solver_tests/*
+if [ $? -ne 0 ]; then
+   echo 'ERROR: pytest failed, exiting ...'
+   exit $?
+fi
 pytest solver_tests_spinn/*
+if [ $? -ne 0 ]; then
+   echo 'ERROR: pytest failed, exiting ...'
+   exit $?
+fi
 echo "Tests done"
 exit 0
