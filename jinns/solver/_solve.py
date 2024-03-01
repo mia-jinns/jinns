aa393269052e95144a0e1be927f0be5ad6eb74b5
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-03-01 15:37:50 +0100

    new version, enhanced for speed. We keep the scan loop as reference but we have a for loop (less efficient, especially on highly fast example such as lotka volterra) for cases when there is devied manipulations

--- jinns/solver/_solve.py
+++ jinns/solver/_solve.py
@@ -6,6 +6,7 @@ handles the optimization process
 from jaxopt import OptaxSolver, LBFGS
 import optax
 from tqdm import tqdm
+from jax_tqdm import scan_tqdm
 import jax
 from jax import jit
 import jax.numpy as jnp
@@ -149,51 +150,24 @@ def solve(
 
     if opt_state is None:
         opt_state = optimizer.init(params)
-    value_grad_loss = jax.value_and_grad(loss, has_aux=True)
 
     curr_seq = 0
     if seq2seq is not None:
-        assert data.method == "uniform", (
-            "data.method must be uniform if" + " using seq2seq learning !"
-        )
+        assert (
+            data.method == "uniform"
+        ), "data.method must be uniform if using seq2seq learning !"
 
-        _update_seq2seq_true = _initialize_seq2seq(loss, data, seq2seq, opt_state)
-
-    else:
-        _update_seq2seq_true = None
-
-    if data.rar_parameters is not None:
-        _rar_step_true, _rar_step_false = _rar_step_init(
-            data.rar_parameters["sample_size"],
-            data.rar_parameters["selected_sample_size"],
-        )
-        data.rar_parameters["iter_from_last_sampling"] = 0
-
-    def get_batch(data, param_data, obs_data):
-        """
-        This function is used at each loop but it cannot be jitted because of
-        device_put
-        """
-        batch = data.get_batch()
-        if param_data is not None:
-            batch = append_param_batch(batch, param_data.get_batch())
-        if obs_data is not None:
-            if obs_batch_sharding is not None:
-                # This is the part that motivated the transition from scan to for loop
-                # Indeed we need to be transit obs_batch from CPU to GPU when we have
-                # huge observations that cannot fit on GPU. Such transfer wasn't meant
-                # to be jitted, i.e. in a scan loop
-                obs_batch = jax.device_put(obs_data.get_batch(), obs_batch_sharding)
-            else:
-                obs_batch = obs_data.get_batch()
-            batch = append_obs_batch(batch, obs_batch)
-        return batch
-
-    # We need to get a loss_term to init stuff
-    batch_ini = get_batch(data, param_data, obs_data)
-    _, loss_terms = loss(params, batch_ini)
+    gradient_step = get_gradient_step_for_loss(optimizer, loss)
+    trigger_rar = get_rar_triggerer(data)
+    total_loss_values = jnp.zeros((n_iter))
+    # depending on obs_batch_sharding we will get the simple get_batch or the
+    # get_batch with device_put, the latter is not jittable
+    get_batch = get_get_batch(obs_batch_sharding)
 
     # initialize the dict for stored parameter values
+    # we need to get a loss_term to init stuff
+    batch_ini, data, param_data, obs_data = get_batch(data, param_data, obs_data)
+    _, loss_terms = loss(params, batch_ini)
     if tracked_params_key_list is None:
         tracked_params_key_list = []
     tracked_params = _tracked_parameters(params, tracked_params_key_list)
@@ -210,10 +184,136 @@ def solve(
         lambda x: jnp.zeros((n_iter)), loss_terms
     )
 
-    total_loss_values = jnp.zeros((n_iter))
+    carry = (
+        init_params,
+        init_params.copy(),
+        data,
+        curr_seq,
+        seq2seq,
+        stored_params,
+        stored_loss_terms,
+        loss,
+        param_data,
+        obs_data,
+        opt_state,
+        total_loss_values,
+    )
+
+    def one_iteration(carry, i):
+        (
+            params,
+            last_non_nan_params,
+            data,
+            curr_seq,
+            seq2seq,
+            stored_params,
+            stored_loss_terms,
+            loss,
+            param_data,
+            obs_data,
+            opt_state,
+            total_loss_values,
+        ) = carry
+        batch, data, param_data, obs_data = get_batch(data, param_data, obs_data)
+
+        (
+            loss_val,
+            loss_terms,
+            params,
+            opt_state,
+            last_non_nan_params,
+        ) = gradient_step(batch, params, opt_state, last_non_nan_params)
+
+        # Print loss during optimization
+        print_fn(i, loss_val, print_loss_every)
+
+        # Trigger RAR
+        loss, params, data = trigger_rar(i, loss, params, data)
+
+        # Trigger seq2seq
+        loss, params, data, opt_state, curr_seq, seq2seq = trigger_seq2seq(
+            i, loss, params, data, opt_state, curr_seq, seq2seq
+        )
+
+        # save loss value and selected parameters
+        stored_params, stored_loss_terms, total_loss_values = store_loss_and_params(
+            i,
+            params,
+            stored_params,
+            stored_loss_terms,
+            total_loss_values,
+            loss_val,
+            loss_terms,
+            tracked_params,
+        )
+        return (
+            params,
+            last_non_nan_params,
+            data,
+            curr_seq,
+            seq2seq,
+            stored_params,
+            stored_loss_terms,
+            loss,
+            param_data,
+            obs_data,
+            opt_state,
+            total_loss_values,
+        ), None
+
+    # Main optimization loop. We use the fully scanned (fully jitted) version
+    # if no mixing devices. Otherwise we use the for loop. Here devices only
+    # concern obs_batch, but it could lead to more complex scheme in the future
+    if obs_batch_sharding is not None:
+        for i in tqdm(range(n_iter)):
+            carry, _ = one_iteration(carry, i)
+    else:
+        carry, _ = jax.lax.scan(
+            scan_tqdm(n_iter)(one_iteration),
+            carry,
+            jnp.arange(n_iter),
+        )
+
+    (
+        init_params,
+        last_non_nan_params,
+        data,
+        curr_seq,
+        seq2seq,
+        stored_params,
+        stored_loss_terms,
+        loss,
+        param_data,
+        obs_data,
+        opt_state,
+        total_loss_values,
+    ) = carry
+
+    jax.debug.print(
+        "Iteration {i}: loss value = {total_loss_val}",
+        i=n_iter,
+        total_loss_val=total_loss_values[-1],
+    )
+
+    return (
+        last_non_nan_params,
+        total_loss_values,
+        stored_loss_terms,
+        data,
+        loss,
+        opt_state,
+        stored_params,
+    )
+
+
+def get_gradient_step_for_loss(optimizer, loss):
+    """
+    A wrapper for loss and optimizer that cannot be jitted
+    """
 
     @jit
     def gradient_step(batch, params, opt_state, last_non_nan_params):
+        value_grad_loss = jax.value_and_grad(loss, has_aux=True)
         (loss_val, loss_terms), grads = value_grad_loss(params, batch)
         updates, opt_state = optimizer.update(grads, opt_state)
         params = optax.apply_updates(params, updates)
@@ -234,118 +334,139 @@ def solve(
             last_non_nan_params,
         )
 
-    @jit
-    def print_fn(i, total_loss_val):
-        # note that if the following is not jitted in the main lor loop, it is
-        # super slow
-        _ = jax.lax.cond(
-            i % print_loss_every == 0,
-            lambda _: jax.debug.print(
-                "Iteration {i}: loss value = {total_loss_val}",
-                i=i,
-                total_loss_val=total_loss_val,
-            ),
-            lambda _: None,
-            (None,),
-        )
+    return gradient_step
 
-    @jit
-    def store_loss_and_params(
-        i,
-        params,
+
+@jit
+def print_fn(i, loss_val, print_loss_every):
+    # note that if the following is not jitted in the main lor loop, it is
+    # super slow
+    _ = jax.lax.cond(
+        i % print_loss_every == 0,
+        lambda _: jax.debug.print(
+            "Iteration {i}: loss value = {loss_val}",
+            i=i,
+            loss_val=loss_val,
+        ),
+        lambda _: None,
+        (None,),
+    )
+
+
+@jit
+def store_loss_and_params(
+    i,
+    params,
+    stored_params,
+    stored_loss_terms,
+    total_loss_values,
+    loss_val,
+    loss_terms,
+    tracked_params,
+):
+    stored_params = jax.tree_util.tree_map(
+        lambda stored_value, param, tracked_param: jax.lax.cond(
+            tracked_param,
+            lambda ope: ope[0].at[i].set(ope[1]),
+            lambda ope: ope[0],
+            (stored_value, param),
+        ),
         stored_params,
+        params,
+        tracked_params,
+    )
+    stored_loss_terms = jax.tree_util.tree_map(
+        lambda stored_term, loss_term: stored_term.at[i].set(loss_term),
         stored_loss_terms,
-        total_loss_values,
-        loss_val,
         loss_terms,
-    ):
-        stored_params = jax.tree_util.tree_map(
-            lambda stored_value, param, tracked_param: jax.lax.cond(
-                tracked_param,
-                lambda ope: ope[0].at[i].set(ope[1]),
-                lambda ope: ope[0],
-                (stored_value, param),
-            ),
-            stored_params,
-            params,
-            tracked_params,
-        )
-        stored_loss_terms = jax.tree_util.tree_map(
-            lambda stored_term, loss_term: stored_term.at[i].set(loss_term),
-            stored_loss_terms,
-            loss_terms,
-        )
+    )
 
-        total_loss_values = total_loss_values.at[i].set(loss_val)
-        return stored_params, stored_loss_terms, total_loss_values
+    total_loss_values = total_loss_values.at[i].set(loss_val)
+    return stored_params, stored_loss_terms, total_loss_values
+
+
+def get_rar_triggerer(data):
+    """
+    A wrapper because the initialization to get _true and _false cannot be
+    jitted
+    """
+    if data.rar_parameters is not None:
+        _rar_step_true, _rar_step_false = _rar_step_init(
+            data.rar_parameters["sample_size"],
+            data.rar_parameters["selected_sample_size"],
+        )
+        data.rar_parameters["iter_from_last_sampling"] = 0
 
     @jit
-    def trigger_rar_seq2seq(i, loss, params, data, opt_state, curr_seq, seq2seq):
-        if seq2seq is not None:
-            loss, params, data, opt_state, curr_seq, seq2seq = _seq2seq_triggerer(
-                loss,
-                params,
-                data,
-                opt_state,
-                curr_seq,
-                seq2seq,
-                i,
-                _update_seq2seq_true,
-                _update_seq2seq_false,
-            )
-        else:
-            curr_seq = -1
-
+    def trigger_rar(i, loss, params, data):
         if data.rar_parameters is not None:
             loss, params, data = _rar_step_triggerer(
                 loss, params, data, i, _rar_step_true, _rar_step_false
             )
+        return loss, params, data
 
-        return loss, params, data, opt_state, curr_seq, seq2seq
+    return trigger_rar
 
-    # Main optimization loop
-    for i in tqdm(range(n_iter)):
-        batch = get_batch(data, param_data, obs_data)
 
-        (
-            loss_val,
-            loss_terms,
+@jit
+def trigger_seq2seq(i, loss, params, data, opt_state, curr_seq, seq2seq):
+    if seq2seq is not None:
+        loss, params, data, opt_state, curr_seq, seq2seq = _seq2seq_triggerer(
+            loss,
             params,
+            data,
             opt_state,
-            last_non_nan_params,
-        ) = gradient_step(batch, params, opt_state, last_non_nan_params)
-
-        # Print loss during optimization
-        print_fn(i, loss_val)
-
-        # Trigger RAR or seq2seq
-        loss, params, data, opt_state, curr_seq, seq2seq = trigger_rar_seq2seq(
-            i, loss, params, data, opt_state, curr_seq, seq2seq
-        )
-
-        # save loss value and selected parameters
-        stored_params, stored_loss_terms, total_loss_values = store_loss_and_params(
+            curr_seq,
+            seq2seq,
             i,
-            params,
-            stored_params,
-            stored_loss_terms,
-            total_loss_values,
-            loss_val,
-            loss_terms,
+            _initialize_seq2seq(loss, data, seq2seq, opt_state),
+            _update_seq2seq_false,
         )
+    else:
+        curr_seq = -1
 
-    jax.debug.print(
-        "Iteration {i}: loss value = {total_loss_val}",
-        i=n_iter,
-        total_loss_val=total_loss_values[-1],
-    )
+    return loss, params, data, opt_state, curr_seq, seq2seq
 
-    return (
-        last_non_nan_params,
-        total_loss_values,
-        stored_loss_terms,
-        data,
-        loss,
-        opt_state,
-        stored_params,
-    )
+
+def get_get_batch(obs_batch_sharding):
+    """
+    Return the get_batch function that will be used either the jittable one or
+    the non-jittable one with sharding
+    """
+
+    def get_batch_sharding(data, param_data, obs_data):
+        """
+        This function is used at each loop but it cannot be jitted because of
+        device_put
+
+        /!\ Return all that's modified or unwanted dirty undefined behaviour
+        """
+        batch = data.get_batch()
+        if param_data is not None:
+            batch = append_param_batch(batch, param_data.get_batch())
+        if obs_data is not None:
+            # This is the part that motivated the transition from scan to for loop
+            # Indeed we need to be transit obs_batch from CPU to GPU when we have
+            # huge observations that cannot fit on GPU. Such transfer wasn't meant
+            # to be jitted, i.e. in a scan loop
+            obs_batch = jax.device_put(obs_data.get_batch(), obs_batch_sharding)
+            batch = append_obs_batch(batch, obs_batch)
+        return batch, data, param_data, obs_data
+
+    @jit
+    def get_batch(data, param_data, obs_data):
+        """
+        Original get_batch with not sharding
+
+        /!\ Return all that's modified or unwanted dirty undefined behaviour
+        """
+        batch = data.get_batch()
+        if param_data is not None:
+            batch = append_param_batch(batch, param_data.get_batch())
+        if obs_data is not None:
+            batch = append_obs_batch(batch, obs_data.get_batch())
+        return batch, data, param_data, obs_data
+
+    if obs_batch_sharding is not None:
+        return get_batch_sharding
+    return get_batch
