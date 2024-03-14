bad8574871966bef7443f2d649bba13c847bff29
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-03-14 11:46:42 +0100

    Add tests for sharding. We fake 2 CPUs as different devices. We test the fact that the sharding version and non sharding version (using respectively for and scan loops) give the same loss results after 10it. These tests modify XLA flags and must be started with a separate pytest command

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -3,6 +3,10 @@ pytest dataGenerator_tests/*
 if [ $? -ne 0 ]; then
    exit $?
 fi
+pytest -s sharding_tests/*
+if [ $? -ne 0 ]; then
+   exit $?
+fi
 pytest solver_tests/*
 if [ $? -ne 0 ]; then
    exit $?
