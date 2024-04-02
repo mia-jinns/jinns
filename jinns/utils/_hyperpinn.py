d933a90be850f4809167837e82e50ff3bbabecb2
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-04-02 19:19:14 +0200

    force the pinn_params_sum and pinn_params_cumsum to be static fields

--- jinns/utils/_hyperpinn.py
+++ jinns/utils/_hyperpinn.py
@@ -9,7 +9,6 @@ import numpy as onp
 import jax
 import jax.numpy as jnp
 from jax.tree_util import tree_leaves, tree_map
-from jax.typing import ArrayLike
 import equinox as eqx
 
 from jinns.utils._pinn import PINN, _MLP
@@ -39,8 +38,8 @@ class HYPERPINN(PINN):
     static_hyper: eqx.Module
     hyperparams: list = eqx.field(static=True)
     hypernet_input_size: int
-    pinn_params_sum: ArrayLike
-    pinn_params_cumsum: ArrayLike
+    pinn_params_sum: onp.ndarray = eqx.field(static=True)
+    pinn_params_cumsum: onp.ndarray = eqx.field(static=True)
 
     def __init__(
         self,
