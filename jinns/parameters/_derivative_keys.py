e8e295c6b542e976e5fd953f1a2e26c19c704207
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-10-11 19:13:33 +0200

    More granularity for derivative keys specification and an example notebook on inverse problem

--- jinns/parameters/_derivative_keys.py
+++ jinns/parameters/_derivative_keys.py
@@ -53,30 +53,15 @@ def _set_derivatives(params, derivative_keys):
     has a copy of the params with appropriate derivatives set
     """
 
-    def _set_derivatives_(loss_term_derivative):
+    def _set_derivatives_(params_, loss_term_derivative):
+        ## If loss_term_derivative is a string
         if loss_term_derivative == "both":
-            return params
+            return params_
         # the next line put a stop_gradient around the fields that do not
         # appear in loss_term_derivative. Currently there are only two possible
-        # values nn_params and eq_params but there might be more in the future
-        return eqx.tree_at(
-            lambda p: tuple(
-                getattr(p, f.name)
-                for f in fields(Params)
-                if f.name != loss_term_derivative
-            ),
-            params,
-            replace_fn=jax.lax.stop_gradient,
-        )
-
-    def _set_derivatives_dict(loss_term_derivative):
-        if loss_term_derivative == "both":
-            return params
-        # the next line put a stop_gradient around the fields that do not
-        # appear in loss_term_derivative. Currently there are only two possible
-        # values nn_params and eq_params but there might be more in the future
-        return {
-            k: eqx.tree_at(
+        # values "nn_params" and "eq_params" but there might be more in the future
+        if isinstance(loss_term_derivative, str):
+            return eqx.tree_at(
                 lambda p: tuple(
                     getattr(p, f.name)
                     for f in fields(Params)
@@ -85,10 +70,32 @@ def _set_derivatives(params, derivative_keys):
                 params_,
                 replace_fn=jax.lax.stop_gradient,
             )
-            for k, params_ in params
+        ## If loss_term_derivative is a Params, to give maximal granularity to
+        # the user to control which term is differentiated
+        if isinstance(loss_term_derivative, Params):
+            return jax.tree.map(
+                lambda p, d: p if d else jax.lax.stop_gradient(p),
+                params_,
+                loss_term_derivative,
+                is_leaf=lambda x: isinstance(x, eqx.Module)
+                and not isinstance(x, Params),
+            )
+        raise ValueError(
+            "Wrong value for loss_term_derivative, it should "
+            "be either a string or a Params object with the same "
+            "structure as Params with True values where derivatives are"
+            " taken and False otherwise"
+        )
+
+    def _set_derivatives_dict(params_, loss_term_derivative):
+        if loss_term_derivative == "both":
+            return params_
+        return {
+            k: _set_derivatives_(params__, loss_term_derivative)
+            for k, params__ in params_
         }
 
     if not isinstance(params, dict):
-        return _set_derivatives_(derivative_keys)
+        return _set_derivatives_(params, derivative_keys)
     else:
-        return _set_derivatives_dict(derivative_keys)
+        return _set_derivatives_dict(params, derivative_keys)
