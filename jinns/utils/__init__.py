ff411887d5f78dba95b4f1c475fcd7a47a99c006
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-11-27 12:22:17 +0100

    new parallel PINN mimicking the PFNN from DeepWDE when we want to compare

--- jinns/utils/__init__.py
+++ jinns/utils/__init__.py
@@ -1,4 +1,5 @@
 from ._pinn import create_PINN, PINN
+from ._ppinn import create_PPINN, PPINN
 from ._spinn import create_SPINN, SPINN
 from ._hyperpinn import create_HYPERPINN, HYPERPINN
 from ._save_load import save_pinn, load_pinn
