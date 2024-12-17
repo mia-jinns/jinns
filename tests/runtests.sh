ece868fbb83e730267cd7d2da89b63370160bb46
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-12-17 11:32:32 +0100

    all the tests run on 2 cpus. One single call to pytest now works to produce coverage

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,51 +1,7 @@
 #!/bin/bash
-cd dataGenerator_tests
-pytest -s
+pytest -s --cov=../jinns --ignore=solver_tests/test_NSPipeFlow_x64.py
 status=$?
 if [ $status -ne 0 ]; then
    exit $status
 fi
-cd ../sharding_tests
-pytest -s
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../save_load_tests
-pytest -s
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../solver_tests
-pytest -s --ignore=test_NSPipeFlow_x64.py
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../solver_tests_spinn
-pytest -s
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../solver_tests_hyperpinn
-pytest -s
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../utils_tests
-pytest -s
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../parameters_tests
-pytest -s
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-cd ../operator_tests
-pytest -s
 exit $?
