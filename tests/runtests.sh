dc03175af8a66a0deb0a604efe42384bea911178
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-12-17 12:21:28 +0100

    coverage test

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-pytest -s --cov=../jinns --ignore=solver_tests/test_NSPipeFlow_x64.py
+pytest -s --cov=/builds/mia_jinns/jinns/jinns --ignore=solver_tests/test_NSPipeFlow_x64.py
+#pytest -s --cov=../jinns --ignore=solver_tests/test_NSPipeFlow_x64.py
 status=$?
 if [ $status -ne 0 ]; then
    exit $status
