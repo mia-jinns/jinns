61c5f3b5c323de2950e621241a6bc89c9803752a
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2024-12-17 13:01:35 +0100

    cov

--- tests/runtests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pytest -s --cov=/builds/mia_jinns/jinns/jinns --ignore=solver_tests/test_NSPipeFlow_x64.py
-#pytest -s --cov=../jinns --ignore=solver_tests/test_NSPipeFlow_x64.py
-status=$?
-if [ $status -ne 0 ]; then
-   exit $status
-fi
-exit $?
