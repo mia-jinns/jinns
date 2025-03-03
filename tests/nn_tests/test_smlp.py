80d2379ef5c4937ce5a2f597ee84016a4d4ca8f2
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2025-03-03 10:41:55 +0100

    add tests for separated MLP architecture (based on MLP)

---
+++ tests/nn_tests/test_smlp.py
@@ -0,0 +1,73 @@
+import pytest
+
+import jax
+import jax.numpy as jnp
+from jax import random
+import equinox as eqx
+import jinns
+
+
+@pytest.fixture
+def create_SMLP():
+    jax.config.update("jax_enable_x64", False)
+    key = random.PRNGKey(2)
+    d = 3
+    r = 256
+    eqx_list = (
+        (eqx.nn.Linear, 1, 128),
+        (jax.nn.tanh,),
+        (eqx.nn.Linear, 128, 128),
+        (jax.nn.tanh,),
+        (eqx.nn.Linear, 128, 128),
+        (jax.nn.tanh,),
+        (eqx.nn.Linear, 128, r),
+    )
+    key, subkey = random.split(key)
+    u, params = jinns.nn.SPINN_MLP.create(subkey, d, r, eqx_list, "nonstatio_PDE")
+
+    return u, params
+
+
+@pytest.fixture
+def create_3_MLPs():
+    """
+    Illustrates the minimal requirements to inherit from PINN
+    """
+    jax.config.update("jax_enable_x64", False)
+
+    key = random.PRNGKey(2)
+    eqx_list = (
+        (eqx.nn.Linear, 1, 128),
+        (jax.nn.tanh,),
+        (eqx.nn.Linear, 128, 128),
+        (jax.nn.tanh,),
+        (eqx.nn.Linear, 128, 128),
+        (jax.nn.tanh,),
+        (eqx.nn.Linear, 128, 256),
+    )
+    eqx_networks = []
+    d = 3
+    key, subkey = random.split(key)  # burn one jey for equivalency
+    keys = jax.random.split(subkey, d)
+    for i in range(d):
+        eqx_networks.append(jinns.nn.MLP(key=keys[i], eqx_list=eqx_list))
+
+    return eqx_networks
+
+
+def test_equality(create_SMLP, create_3_MLPs):
+    u1, p1 = create_SMLP
+    eqx_networks = create_3_MLPs
+    key = random.PRNGKey(2)
+    key, subkey = jax.random.split(key, 2)
+    test_points = jax.random.normal(subkey, shape=(10, 3))
+    results1 = jax.vmap(eqx.combine(p1, u1.static))(test_points)
+    results2 = [
+        jax.vmap(u)(test_points[:, idx][:, None]) for idx, u in enumerate(eqx_networks)
+    ]
+    print(results1, results2)
+    assert jnp.allclose(
+        jnp.array(results1),
+        jnp.swapaxes(jnp.array(results2), 0, 1),
+        atol=1e-3,
+    )
