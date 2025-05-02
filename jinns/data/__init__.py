7198e0c216d4cb0996b87b651fa80bb9f40582f1
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2025-05-02 11:04:16 +0200

    refactor the module

--- jinns/data/__init__.py
+++ jinns/data/__init__.py
@@ -1,10 +1,8 @@
-from ._DataGenerators import (
-    DataGeneratorODE,
-    CubicMeshPDEStatio,
-    CubicMeshPDENonStatio,
-    DataGeneratorObservations,
-    DataGeneratorParameter,
-)
+from ._DataGeneratorODE import DataGeneratorODE
+from ._CubicMeshPDEStatio import CubicMeshPDEStatio
+from ._CubicMeshPDENonStatio import CubicMeshPDENonStatio
+from ._DataGeneratorObservations import DataGeneratorObservations
+from ._DataGeneratorParameter import DataGeneratorParameter
 from ._Batchs import ODEBatch, PDEStatioBatch, PDENonStatioBatch
 
-from ._DataGenerators import append_obs_batch, append_param_batch
+from ._utils import append_obs_batch, append_param_batch
