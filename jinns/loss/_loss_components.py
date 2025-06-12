2990c52bf7ca2febfc43c02d0d88a5b1d08568d2
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2025-06-12 17:35:10 +0200

    add a custom items() method to loss components so that they can be treated as dictionaries

--- jinns/loss/_loss_components.py
+++ jinns/loss/_loss_components.py
@@ -1,10 +1,11 @@
 from typing import TypeVar, Generic
+from dataclasses import fields
 import equinox as eqx
 
 T = TypeVar("T")
 
 
-class ODEComponents(eqx.Module, Generic[T]):
+class XDEComponentsAbstract(eqx.Module, Generic[T]):
     """
     Provides a template for ODE components with generic types.
     One can inherit to specialize and add methods and attributes
@@ -12,12 +13,26 @@ class ODEComponents(eqx.Module, Generic[T]):
     work like a tuple)
     """
 
+    def items(self):
+        """
+        For the dataclass to be iterated like a dictionary.
+        Practical and retrocompatible with old code when loss components were
+        dictionaries
+        """
+        return {
+            field.name: getattr(self, field.name)
+            for field in fields(self)
+            if getattr(self, field.name) is not None
+        }.items()
+
+
+class ODEComponents(XDEComponentsAbstract[T]):
     dyn_loss: T
     initial_condition: T
     observations: T
 
 
-class PDEStatioComponents(eqx.Module, Generic[T]):
+class PDEStatioComponents(XDEComponentsAbstract[T]):
     dyn_loss: T
     norm_loss: T
     boundary_loss: T
