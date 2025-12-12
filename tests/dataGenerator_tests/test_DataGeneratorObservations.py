256dcdd1038717d6ceaa779116945013020379ad
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2025-12-12 12:53:03 +0100

    wip on a test to evaluate memory gain with unduplicated DGObs

--- tests/dataGenerator_tests/test_DataGeneratorObservations.py
+++ tests/dataGenerator_tests/test_DataGeneratorObservations.py
@@ -1,3 +1,10 @@
+import os
+
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
+import time
+from itertools import zip_longest
+from functools import partial
 import pytest
 import jax
 import jax.numpy as jnp
@@ -171,3 +178,140 @@ def test_batch_size_checks():
             observed_values=(jnp.ones((100, 3)), jnp.ones((10, 3)), jnp.ones((10, 3))),
             obs_batch_size=(None, 10),
         )
+
+
+def test_batch_equality():
+    """
+    To avoid problems of jitting a bound method, we decorticated the get_bathc
+    function of DataGeneratorObservations here
+    """
+
+    def get_obs_batch_fun(dg):
+        args = (
+            dg.observed_eq_params,
+            dg.observed_pinn_in,
+            dg.observed_values,
+            dg.n,
+            dg.obs_batch_size,
+            dg.curr_idx,
+            dg.key,
+            dg.indices,
+        )
+
+        tree_map_args = tuple(
+            (
+                {"observed_eq_params": e},
+                {"observed_pinn_in": p},
+                {"observed_values": v},
+                {"n": n},
+                {"obs_batch_size": b},
+                {"curr_idx": c},
+                {"key": k},
+                {"indices": i},
+            )
+            for e, p, v, n, b, c, k, i in zip_longest(*args)
+        )
+        fixed_args = ()
+        obs_batch_fun = jinns.data.DataGeneratorObservations.obs_batch
+        if len(set(map(len, args))) > 1:  # at least 2 lengths differ
+            # but since values, pinn_in and equations are the arguments that
+            # generates all the others, it suffices to potentially fix the
+            # former
+            if len(dg.observed_pinn_in) == 1:
+                obs_batch_fun = partial(
+                    obs_batch_fun, observed_pinn_in=dg.observed_pinn_in[0]
+                )
+                fixed_args = fixed_args + ("observed_pinn_in",)
+            if len(dg.observed_eq_params) == 1:
+                obs_batch_fun = partial(
+                    obs_batch_fun, observed_eq_params=dg.observed_eq_params[0]
+                )
+                fixed_args = fixed_args + ("observed_eq_params",)
+            if len(dg.observed_values) == 1:
+                obs_batch_fun = partial(
+                    obs_batch_fun, observed_values=dg.observed_values[0]
+                )
+                fixed_args = fixed_args + ("observed_values",)
+
+        fun = lambda args: jax.tree.map(
+            jinns.data._DataGeneratorObservations._merge_dict_arguments(
+                obs_batch_fun, fixed_args
+            ),
+            args,
+            is_leaf=lambda x: (isinstance(x, tuple) and isinstance(x[0], dict)),
+        )
+        return fun, tree_map_args
+
+    #### FIRST a cost_analysis for the duplicated case
+    key = jax.random.PRNGKey(2)
+    key, subkey = jax.random.split(key)
+    subkeys2 = jax.random.split(key, 20)
+
+    print(jax.random.normal(key, shape=(100000, 3)).nbytes)
+
+    print("Before duplicated DG instanciation")
+    time.sleep(5)
+    dg1 = jinns.data.DataGeneratorObservations(
+        key=subkey,
+        observed_pinn_in=tuple(
+            jax.random.normal(key, shape=(100000, 3)) for _ in range(len(subkeys2))
+        ),
+        observed_values=tuple(
+            jax.random.normal(k, shape=(100000, 15)) for k in subkeys2
+        ),
+        observed_eq_params=tuple(
+            {"a": jax.random.normal(key, shape=(100000,))} for _ in range(len(subkeys2))
+        ),
+    )
+    print("After duplicated DG instanciation")
+    print("Before unduplicated DG instanciation")
+    time.sleep(5)
+    #  print(sys.getsizeof(dg))
+    #  #print(jnp.ones(1000).nbytes)
+    #  fun, tree_map_args = get_obs_batch_fun(dg1)
+    #  compiled_1 = jax.jit(fun).lower(
+    #      tree_map_args
+    #  ).compile()
+    #  print(compiled_1.cost_analysis()["bytes accessed"])
+    #  obs_batch_duplicated = jax.tree.map(
+    #      lambda l:l[3],
+    #      compiled_1(tree_map_args),
+    #      is_leaf=lambda x: isinstance(x, tuple) and len(x)==4
+    #  )
+
+    #### SECOND a cost_analysis for the non duplicated case
+    key = jax.random.PRNGKey(2)
+    key, subkey = jax.random.split(key)
+
+    dg2 = jinns.data.DataGeneratorObservations(
+        key=subkey,
+        observed_pinn_in=jax.random.normal(key, shape=(100000, 3)),
+        observed_values=tuple(
+            jax.random.normal(k, shape=(100000, 15)) for k in subkeys2
+        ),
+        observed_eq_params={"a": jax.random.normal(key, shape=(100000,))},
+    )
+
+    print("After unduplicated DG instanciation")
+    time.sleep(5)
+
+
+#   fun, tree_map_args = get_obs_batch_fun(dg2)
+#   compiled_2 = jax.jit(fun).lower(
+#       tree_map_args
+#   ).compile()
+#   print(compiled_2.cost_analysis()["bytes accessed"])
+#   obs_batch_nonduplicated = jax.tree.map(
+#       lambda l:l[3],
+#       compiled_2(tree_map_args),
+#       is_leaf=lambda x: isinstance(x, tuple) and len(x)==4
+#   )
+
+#   assert all(tuple(jnp.allclose(
+#       obs_batch_duplicated[i]["pinn_in"],
+#       obs_batch_nonduplicated[i]["pinn_in"]
+#   ) for i in range(len(obs_batch_duplicated))))
+#   assert all(tuple(jnp.allclose(
+#       obs_batch_duplicated[i]["val"],
+#       obs_batch_nonduplicated[i]["val"]
+#   ) for i in range(len(obs_batch_duplicated))))
