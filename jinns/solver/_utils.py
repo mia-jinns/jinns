cb41cd0ff582b97c9782077f3474d3e112fdb005
GANGLOFF Hugo <hugo.gangloff@inrae.fr>
2026-04-10 16:35:50 +0200

    working example with new jinns solve argument hardcoded in solve/utils

--- jinns/solver/_utils.py
+++ jinns/solver/_utils.py
@@ -7,7 +7,6 @@ from __future__ import (
 )  # https://docs.python.org/3/library/typing.html#constant
 
 from typing import TYPE_CHECKING, Callable
-import inspect
 import jax
 from jax import jit
 import jax.numpy as jnp
@@ -207,51 +206,80 @@ def _loss_evaluate_and_euclidean_gradient_step(
     # 3. total loss after possible weight update
     train_loss_value = loss.ponderate_and_sum_loss(loss_terms)
 
-    extra_args_optax_keys = {} # the extra_args dict that is used in optax
-    extra_args_keys = []
-    extra_args_to_jinns_locals = {
+    # extra_args_optax_keys = {} # the extra_args dict that is used in optax
+    # extra_args_keys = []
+    # extra_args_to_jinns_locals = {
+    #    "value": "train_loss_value",
+    #    "value_fn": ("loss", "evaluate"),
+    #    "grad": "params",
+    #    "batch": "batch"
+    # }
+    # if 'extra_args' in inspect.signature(optimizer.update).parameters.keys():
+    #    # possible cases either a single optax transform with extra args
+    #    # or a chain of optax transforms with or without extra args
+    #    extra_args_keys = []
+    #    if 'update_fns' in inspect.getclosurevars(optimizer.update).nonlocals.keys():
+    #        # this is a chain optax transform
+    #        # get the arguments of each update_fn
+    #        for update_fn_inside_chain in inspect.getclosurevars(optimizer.update).nonlocals['update_fns']:
+    #            # first three args are always 'updates', 'state', 'params'
+    #            # and they don't interest us
+    #            extra_args_keys += list(
+    #                inspect.signature(update_fn_inside_chain).parameters.keys()
+    #            )[3:]
+    #    # custom rule for jinns: if value_fn is require (ie loss.evaluate) then
+    #    # batch is necesarily an extra_arg since we always call
+    #    # loss.evaluate(params, batch)
+    # else:
+    #    # this can be optax transform with extra args without extra_args named!
+    #    extra_args_for_update_fn =
+
+    # Below are the stuff passed by the user when they see they have an optax
+    # transform with extra args. The example below enables using optax.lbfgs
+    # whose update extra args contain in order: value, grad, value_fn,
+    # extra_kwargs (for value_fn)
+    # the user should then look for the corresponding variable in jinns
+    # loss_evaluate_and_standard_gradient and pass the variable name as a
+    # string
+    extra_optax_args_and_kwargs_in_jinns_locals = {
         "value": "train_loss_value",
-        "value_fn": ("loss", "evaluate"),
         "grad": "params",
-        "batch": "batch"
+        "value_fn": "lambda params, batch: loss.evaluate(params, batch)[0]",
+        "batch": "batch",
     }
+    # extra_kwargs_optax_in_jinns_locals = {"batch": "batch"}
 
-    if 'extra_args' in inspect.signature(optimizer.update).parameters.keys():
-        # possible cases either a single optax transform with extra args
-        # or a chain of optax transforms with or without extra args
-        extra_args_keys = []
-        if 'update_fns' in inspect.getclosurevars(optimizer.update).nonlocals.keys():
-            # this is a chain optax transform
-            # get the arguments of each update_fn
-            for update_fn_inside_chain in inspect.getclosurevars(optimizer.update).nonlocals['update_fns']:
-                # first three args are always 'updates', 'state', 'params'
-                # and they don't interest us
-                extra_args_keys += list(
-                    inspect.signature(update_fn_inside_chain).parameters.keys()
-                )[3:]
-        # custom rule for jinns: if value_fn is require (ie loss.evaluate) then
-        # batch is necesarily an extra_arg since we always call
-        # loss.evaluate(params, batch)
-    else:
-        # this can be optax transform with extra args without extra_args named!
-        extra_args_for_update_fn = 
+    # TODO use the above code to ensure user's dictionary match what's expected
+    # by optax
 
+    extra_args_and_kwargs_for_update_fn = {}
+    for kw, variable_name in extra_optax_args_and_kwargs_in_jinns_locals.items():
+        jinns_local_var = eval(variable_name, locals())
+        # var_name_components = variable_name.split(".")
+        # try:
+        #    jinns_local_var = locals()[var_name_components[0]]
+        #    # we must access attributes if len(var_name_component) > 1
+        #    for var_name_component in var_name_components[1:]:
+        #        jinns_local_var = getattr(jinns_local_var, var_name_component)
+        # except KeyError as e:
+        #    raise ValueError(
+        #        f"No local variable corresponding to" f"{variable_name}"
+        #    ) from e
+        extra_args_and_kwargs_for_update_fn[kw] = jinns_local_var
 
-    extra_args_for_update_fn = {}
-    for key_ in extra_args_keys:
-        jinns_local_name = extra_args_to_jinns_locals[key_]
-        if isinstance(jinns_local_name, str):
-            jinns_local_var = locals()[jinns_local_name]
-        elif isinstance(jinns_local_name, tuple):
-            # we must access attributes
-            jinns_local_var = locals()[jinns_local_name[0]]
-            for attr in jinns_local_name[1:]:
-                jinns_local_var = getattr(jinns_local_var, "attr")
-        else:
-            raise ValueError("Wrong correspondency for {key} to jinns local"
-            " var")
-        extra_args_for_update_fn[key] = jinns_local_var
-
+    # extra_kwargs_for_update_fn = {}
+    # for kw, variable_name in extra_kwargs_optax_in_jinns_locals.items():
+    #    var_name_components = variable_name.split(".")
+    #    try:
+    #        jinns_local_var = locals()[var_name_components[0]]
+    #        # we must access attributes if len(var_name_component) > 1
+    #        for var_name_component in var_name_components[1:]:
+    #            jinns_local_var = getattr(jinns_local_var, var_name_component)
+    #    except KeyError as e:
+    #        raise ValueError(
+    #            f"No local variable corresponding to" f"{variable_name}"
+    #        ) from e
+    #    extra_kwargs_for_update_fn[kw] = jinns_local_var
 
     params, state = _gradient_step(
         grads,
@@ -260,7 +288,7 @@ def _loss_evaluate_and_euclidean_gradient_step(
         # params here, this would be a wrong procedure
         state,
         params_mask,
-        **extra_args_for_update_fn
+        **extra_args_and_kwargs_for_update_fn,
     )
 
     # check if any of the parameters is NaN
@@ -396,6 +424,7 @@ def _gradient_step(
     params: Params[Array],
     state: optax.OptState,
     params_mask: Params[bool] | None = None,
+    *args,
     **kwargs,
 ) -> tuple[
     Params[Array],
@@ -450,6 +479,7 @@ def _gradient_step(
         opt_grads,  # type: ignore
         state,
         params,  # type: ignore
+        *args,
         **kwargs,
     )  # Also see optimizer.init for explanation of type ignore
 
