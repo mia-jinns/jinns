pass the correct gradient argument to callback functions

tests/alternate_solver_tests/test_Burgers_inverse_solve_alternate.py
@@ -201,8 +201,8 @@ def train_solve_alternate_ssbroyden_adam(test_init):
         grads = loss.ponderate_and_sum_gradient(grads)
         return eqx.partition(grads, params_mask_)[0]
 
-    def get_grad_pt(params, params_mask):
-        return params.partition(params_mask)[0]
+    def get_grad_pt(grads, params_mask):
+        return grads.partition(params_mask)[0]
 
     def get_non_opt_params(params, params_mask):
         return eqx.partition(params, params_mask)[1]
@@ -251,4 +251,4 @@ def train_solve_alternate_ssbroyden_adam(test_init):
 def test_ssbroyden_and_adam(train_solve_alternate_ssbroyden_adam):
     total_loss_values = train_solve_alternate_ssbroyden_adam
 
-    assert jnp.allclose(total_loss_values[-1], 0.7067825, atol=1e-5)
+    assert jnp.allclose(total_loss_values[-1], 0.70643278, atol=1e-5)

tests/optimizer_tests/test_lbfgs.py
@@ -103,7 +103,7 @@ def test_heat_ngd_10it(train_heat_init):
 
     extra_optax_args_and_kwargs = {
         "value": jinns.solver.GetJinnsVariableName("train_loss_value"),
-        "grad": jinns.solver.GetJinnsVariableName("params"),
+        "grad": jinns.solver.GetJinnsVariableName("grads"),
         "value_fn": callback_value_fn,
         "batch": jinns.solver.GetJinnsVariableName("batch"),
         "loss": jinns.solver.GetJinnsVariableName("loss"),
@@ -133,4 +133,4 @@ def test_heat_ngd_10it(train_heat_init):
         extra_optax_args_and_kwargs=extra_optax_args_and_kwargs,
     )
 
-    assert jnp.allclose(total_loss_list[-1], 0.22496643, atol=1e-6)
+    assert jnp.allclose(total_loss_list[-1], 0.22491671, atol=1e-6)